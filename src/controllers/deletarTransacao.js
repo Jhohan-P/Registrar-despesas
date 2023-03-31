ee858b55e53af6aea55448918009da76f2cc80d4
Jhohan <jhohan.patrick1@gmail.com>
2023-03-31 20:56:09 -0300

    implemetando funcao de deletar transacao

---
+++ src/controllers/deletarTransacao.js
@@ -0,0 +1,25 @@
+const knex = require('../connections/pg')
+
+const deletarTransacao = async (req, res) => {
+    const usuario = req.usuario
+    const { id } = req.params
+
+    try {
+        const deletarTransacao = await knex('transacoes').where({ id, usuario_id: usuario.id }).del()
+
+        if (deletarTransacao.length == 0) {
+            return res.status(404).json(
+                {
+                    "mensagem": "Transação não encontrada."
+                }
+            );
+        };
+
+        res.status(200).send();
+
+    } catch (error) {
+        return res.status(500).json({ "messagem": "Erro interno do servidor" });
+    };
+};
+
+module.exports = deletarTransacao
