4d7e808d2b2c3deb7ccab94b4dd2aed166d01af9
Jhohan <jhohan.patrick1@gmail.com>
2023-03-31 21:00:31 -0300

    corrigido alguns erros

--- src/controllers/listaTransacaoUsuarioLogado.js
+++ src/controllers/listaTransacaoUsuarioLogado.js
@@ -1,6 +1,5 @@
 const knex = require('../connections/pg')
 
-
 const listarTransacaoUsuarioLogado = async (req, res) => {
     const { id } = req.usuario
     const filtro = req.query.filtro
@@ -12,7 +11,6 @@ const listarTransacaoUsuarioLogado = async (req, res) => {
                 .select('t.id as id', 't.tipo', 't.descricao', 't.valor', 't.data', 't.usuario_id', 't.categoria_id', 'c.descricao as categoria_nome')
                 .join('categorias as c', 't.categoria_id', '=', 'c.id')
                 .where('t.usuario_id', '=', id);
-            // const { rows } = await conexao("select t.id as id, t.tipo, t.descricao, t.valor, t.data, t.usuario_id, t.categoria_id, c.descricao as categoria_nome from transacoes t join categorias c on t.categoria_id = c.id where usuario_id = $1", [usuario.id]);
 
             return res.json(transacaoComFiltro);
         }
@@ -25,11 +23,8 @@ const listarTransacaoUsuarioLogado = async (req, res) => {
                 .join('categorias as c', 't.categoria_id', '=', 'c.id')
                 .where('t.usuario_id', '=', id)
                 .andWhereRaw('lower(c.descricao) ilike any(?)', [`{${arrayFiltro.join(',')}}`]);
-        // const semFiltro = await knex.select('id', 'descricao', 'valor', 'data', 'usuario_id', 'categoria_id').from('transacoes').join('categoria', { 'categoria.id': 'trasacoes.categoria_id' }).where({ id })
-        // const { rows } = await conexao("select t.id as id, t.tipo, t.descricao, t.valor, t.data, t.usuario_id, t.categoria_id, c.descricao as categoria_nome from transacoes t join categorias c on t.categoria_id = c.id where usuario_id = $1 and lower(c.descricao) ilike any($2::Text[])", [usuario.id, arrayFiltro]);
 
         return res.json(transacaoSemFiltro);
-
 
     } catch (error) {
         return res.status(500).json({ "messagem": "Erro interno do servidor" });
