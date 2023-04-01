706b70f26170f7102cbd470558f8b1df644fef76
Jhohan <jhohan.patrick1@gmail.com>
2023-03-31 21:14:12 -0300

    criado atulizar transacao

---
+++ src/controllers/atualizarTransacao.js
@@ -0,0 +1,39 @@
+const knex = require('../connections/pg')
+
+const atualizarTransacao = async (req, res) => {
+    const { descricao, valor, data, categoria_id, tipo } = req.body
+    const { id } = req.params
+    const usuario = req.usuario
+
+    try {
+
+        const transacao = await knex('transacoes').where({ id })
+        // const transacao = await knex('select *from transacoes where id =$1 and usuario_id=$2', [id, usuario.id]);
+
+        if (transacao.length == 0) {
+            return res.status(404).json(
+                {
+                    "mensagem": "Transação não encontrada."
+                }
+            );
+        };
+        if (transacao[0].usuario_id !== usuario.id) {
+            return res.status(404).json(
+                {
+                    "mensagem": "Nao e possivel atualixar uma transacao que nao e sua."
+                }
+            );
+
+        }
+
+        const atulizacao = await knex('transacoes').where({ id }).update({ descricao: descricao, valor: valor, data: data, categoria_id: categoria_id, tipo: tipo });
+
+        return res.status(200).send();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ "messagem": "Erro interno do servidor" });
+    };
+};
+
+module.exports = atualizarTransacao
